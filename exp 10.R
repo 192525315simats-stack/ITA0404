1a04aabe0b2d7cea36edab814cf8b849aa5d0f62
192525315simats-stack <192525315.simats@saveetha.com>
2026-08-03 10:04:49 +0530

    Add files via upload

---
+++ exp 10.R
@@ -0,0 +1,12 @@
+student <- data.frame(
+  Name=c("Ram","Sita","Ravi"),
+  Score=c(90,85,80),
+  Attempts=c(1,2,1),
+  Qualify=c("Yes","Yes","No")
+)
+
+print(student)
+
+student$Age <- c(20,21,22)
+
+print(student)
