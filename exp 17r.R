82a61687db07bc7ee1d63498c47f5a308873dae8
192525315simats-stack <192525315.simats@saveetha.com>
2026-09-02 13:38:48 +0530

    Add files via upload

---
+++ exp 17r.R
@@ -0,0 +1,48 @@
+data(Titanic)
+
+# Convert Titanic table to data frame
+titanic_data <- as.data.frame(Titanic)
+
+# Display
+print(titanic_data)
+
+# Survival vs Class
+survival_class <- xtabs(
+  Freq ~ Class + Survived,
+  data = titanic_data
+)
+
+barplot(
+  survival_class,
+  beside = TRUE,
+  legend = TRUE,
+  main = "Survival vs Class",
+  xlab = "Class",
+  ylab = "Number of Passengers"
+)
+
+# Survival vs Class by Gender
+survival_gender <- xtabs(
+  Freq ~ Class + Survived + Sex,
+  data = titanic_data
+)
+
+# Male
+barplot(
+  survival_gender[, , "Male"],
+  beside = TRUE,
+  legend = TRUE,
+  main = "Survival by Class - Male",
+  xlab = "Class",
+  ylab = "Passengers"
+)
+
+# Female
+barplot(
+  survival_gender[, , "Female"],
+  beside = TRUE,
+  legend = TRUE,
+  main = "Survival by Class - Female",
+  xlab = "Class",
+  ylab = "Passengers"
+)
